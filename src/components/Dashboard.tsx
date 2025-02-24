838445b034a853f7790cfdd3b16f27f855fa98a6
HerbertKayky <herbertkayky9@gmail.com>
2025-02-24 10:45:37 -0300

    Adiciona skeleton de loading ao dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -22,10 +22,6 @@ export function Dashboard() {
     setLoading(false);
   }
 
-  if (loading) {
-    return <div>Carregando...</div>;
-  }
-
   return (
     <div className="py-4">
       <div className="pt-2">
@@ -36,27 +32,41 @@ export function Dashboard() {
         <h1 className="font-bold text-lg">Os mais pedidos</h1>
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
-          {burgers.map((item) => (
-            <div key={item.id} className="bg-gray-100 p-2 rounded-lg">
-              <Link href={`/burgers/${item.id}`}>
-                <Image
-                  width={1000}
-                  height={1000}
-                  src={item.image || ""}
-                  alt={item.name}
-                  className="w-full h-36 object-cover rounded-md"
-                />
-              </Link>
-              <p className="text-sm font-semibold mt-2">{item.name}</p>
-              <p className="text-xs text-gray-600">R$ {item.price}</p>
-              <button
-                onClick={() => addToCart({ ...item, id: item.id, quantity: 1 })}
-                className="mt-2 w-full py-1 bg-blue-500 text-white rounded-md"
-              >
-                Adicionar ao Carrinho
-              </button>
-            </div>
-          ))}
+          {loading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <div
+                  key={index}
+                  className="bg-gray-200 p-2 rounded-lg animate-pulse"
+                >
+                  <div className="w-full h-36 bg-gray-300 rounded-md"></div>
+                  <div className="h-4 bg-gray-300 mt-2 rounded"></div>
+                  <div className="h-4 bg-gray-300 mt-1 w-1/2 rounded"></div>
+                  <div className="h-8 bg-gray-300 mt-2 rounded"></div>
+                </div>
+              ))
+            : burgers.map((item) => (
+                <div key={item.id} className="bg-gray-100 p-2 rounded-lg">
+                  <Link href={`/burgers/${item.id}`}>
+                    <Image
+                      width={1000}
+                      height={1000}
+                      src={item.image || ""}
+                      alt={item.name}
+                      className="w-full h-36 object-cover rounded-md"
+                    />
+                  </Link>
+                  <p className="text-sm font-semibold mt-2">{item.name}</p>
+                  <p className="text-xs text-gray-600">R$ {item.price}</p>
+                  <button
+                    onClick={() =>
+                      addToCart({ ...item, id: item.id, quantity: 1 })
+                    }
+                    className="mt-2 w-full py-1 bg-blue-500 text-white rounded-md"
+                  >
+                    Adicionar ao Carrinho
+                  </button>
+                </div>
+              ))}
         </div>
       </div>
 
@@ -64,28 +74,45 @@ export function Dashboard() {
         <h1 className="font-bold text-lg">Burgers Artesanais</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-2">
-          {burgers.map((item) => (
-            <div
-              key={item.id}
-              className="flex border-gray-300 border rounded-lg"
-            >
-              <Link className="flex items-center" href={`/burgers/${item.id}`}>
-                <Image
-                  width={1000}
-                  height={1000}
-                  src={item.image || ""}
-                  alt={item.name}
-                  className="w-full h-36 object-contain rounded-md"
-                />
-
-                <div className="flex flex-col gap-2 p-4">
-                  <h1 className=" font-semibold mt-2">{item.name}</h1>
-                  <p className=" text-gray-600">{item.description}</p>
-                  <p className="font-bold text-gray-600">R$ {item.price}</p>
+          {loading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <div
+                  key={index}
+                  className="flex border-gray-300 border rounded-lg animate-pulse"
+                >
+                  <div className="w-36 h-36 bg-gray-300 rounded-md"></div>
+                  <div className="flex flex-col gap-2 p-4">
+                    <div className="h-4 bg-gray-300 w-3/4 rounded"></div>
+                    <div className="h-4 bg-gray-300 w-1/2 rounded"></div>
+                    <div className="h-4 bg-gray-300 w-1/4 rounded"></div>
+                  </div>
                 </div>
-              </Link>
-            </div>
-          ))}
+              ))
+            : burgers.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex border-gray-300 border rounded-lg"
+                >
+                  <Link
+                    className="flex items-center"
+                    href={`/burgers/${item.id}`}
+                  >
+                    <Image
+                      width={1000}
+                      height={1000}
+                      src={item.image || ""}
+                      alt={item.name}
+                      className="w-36 h-36 object-contain rounded-md"
+                    />
+
+                    <div className="flex flex-col gap-2 p-4">
+                      <h1 className="font-semibold mt-2">{item.name}</h1>
+                      <p className="text-gray-600">{item.description}</p>
+                      <p className="font-bold text-gray-600">R$ {item.price}</p>
+                    </div>
+                  </Link>
+                </div>
+              ))}
         </div>
       </div>
     </div>
